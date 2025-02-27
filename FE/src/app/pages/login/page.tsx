806dcb852afd8b26c1fd29239a5301f2c97a4ba5
Ricardo Erazo <erazo.14@hotmail.com>
2025-02-27 11:10:21 -0600

    feat: adding react project

---
+++ FE/src/app/pages/login/page.tsx
@@ -0,0 +1,10 @@
+import styles from "./login.module.css";
+
+export default function Home() {
+    return (
+        <div className={styles.page}>
+            <div>hola
+            </div>
+        </div>
+    )
+}
